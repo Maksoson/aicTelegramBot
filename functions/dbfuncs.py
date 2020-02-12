e26b458d429ce126f0131cc68e570c3e35649ce0
Максим Краснопер <maxibon_17@mail.ru>
2020-02-12 18:10:22 +0300

    update bot

--- functions/dbfuncs.py
+++ functions/dbfuncs.py
@@ -87,7 +87,7 @@ class DatabaseFuncs:
         with closing(self.getConnection()) as connection:
             with connection.cursor() as cursor:
                 query = 'INSERT INTO public.timetable (user_id, day_use, start_time, end_time, date_create, date_update) VALUES (%s, %s, %s, %s, ' \
-                        'to_timestamp(%s,"YYYY/MM/DD HH24:MI:SS"), to_timestamp(%s,"YYYY/MM/DD HH24:MI:SS"))'
+                        'to_timestamp("%s","YYYY/MM/DD HH24:MI:SS"), to_timestamp("%s","YYYY/MM/DD HH24:MI:SS"))'
                 cursor.execute(query, (self.getUserId(message), datetime.datetime.today().day,
                                        collection['start_time'], collection['end_time'],
                                        str(datetime.datetime.today()), str(datetime.datetime.today())))
