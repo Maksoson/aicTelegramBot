855cc052c251ca520eddac8ccfb1a956556eedac
Максим Краснопер <maxibon_17@mail.ru>
2020-02-13 12:10:38 +0300

    1000000 commits sry

--- functions/dbfuncs.py
+++ functions/dbfuncs.py
@@ -86,9 +86,10 @@ class DatabaseFuncs:
         with closing(self.getConnection()) as connection:
             with connection.cursor() as cursor:
                 query = 'INSERT INTO public.timetable (user_id, day_use, start_time, end_time, date_create, date_update) ' \
-                        'VALUES (%s, %s, %s, %s, %s, %s)'
+                        'VALUES (%s, %s, %s, %s, %s::timestamp, %s::timestamp)'
                 date_create = time.time()
                 date_update = date_create
+                print("ALO + " + str(self.getUserId(message)))
                 cursor.execute(query, (self.getUserId(message), datetime.datetime.today().day,
                                        collection['start_time'], collection['end_time'],
                                        date_create, date_update))
