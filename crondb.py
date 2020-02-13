7afc14ca283a16526869f20edaef3bd2d16a2e2c
Краснопер Максим <maxibon_17@mail.ru>
2020-02-13 22:04:01 +0300

    10000 commits sry

--- crondb.py
+++ crondb.py
@@ -11,13 +11,13 @@ bot = telebot.TeleBot(bot_home.getBot)
 scheduler = BlockingScheduler()
 db_funcs = dbfuncs.DatabaseFuncs(bot)
 
-@scheduler.scheduled_job('interval', minutes=2)
-def timed_job():
-    print("Otrabotalo")
-    db_funcs.deleteOldTimes()
+# @scheduler.scheduled_job('interval', minutes=2)
+# def timed_job():
+#     print("Otrabotalo")
+#     db_funcs.deleteOldTimes()
 
 
-@scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=23)
+@scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=23, minutes=59)
 def scheduled_job():
     print("Cron otrabotal")
     db_funcs.deleteOldTimes()
