74038b96533ff556e1355f8657d69b0cfe932b7f
Максим Краснопер <maxibon_17@mail.ru>
2020-02-13 16:47:05 +0300

    1000000 commits sry

--- functions/botfuncs.py
+++ functions/botfuncs.py
@@ -39,13 +39,7 @@ class BotFuncs:
         data = self.db_funcs.getAllTimes(today.day)
         counter = 1
         for row in data:
-            user_name = '' if row[1] is None else row[1]
-            first_name = '' if row[2] is None else row[2]
-            # last_name = '' if row[3] is None else row[3]
-
-            last_name = str(row[3])
-
             result_list += str(counter) + '. ' + str(row[11]).strip() + ' - ' + str(row[12]).strip() + '; ' \
-                           + str(first_name).strip() + ' ' + str(last_name).strip() + ' (@' + str(user_name).strip() + ')\n'
+                           + str(row[2]).strip() + ' ' + str(row[3]).strip() + ' (@' + str(row[1]).strip() + ')\n'
             counter += 1
         self.bot.send_message(message.chat.id, result_list)
