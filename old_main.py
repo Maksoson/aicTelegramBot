1ee2f2ccd9799455f132c0563b36b67eb2bccd6e
Максим Краснопер <maxibon_17@mail.ru>
2020-02-20 20:16:38 +0300

    10000 commits lets go

--- old_main.py
+++ old_main.py
@@ -1,79 +1,119 @@
-import requests
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import telebot
 import datetime
 from datafiles import config
-# import pb
-import pytz
+import os
+import bothome
+from flask import Flask, request
 
-P_TIMEZONE = pytz.timezone(config.TIMEZONE)
-TIMEZONE_COMMON_NAME = config.TIMEZONE_COMMON_NAME
-
-class BotHandler:
-
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
-
-    def get_updates(self, timeout=100, offset=None):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
-
-    def send_message(self, chat_id, message):
-        params = {'chat_id': chat_id, 'text': message}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
-
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
+from functions import dbfuncs, botfuncs
 
 
-token = config.TOKEN
-greet_bot = BotHandler(token)
-greetings = {'здравствуй', 'привет', 'ку'}
-now = datetime.datetime.now()
+bot = telebot.TeleBot(config.TOKEN)
+bot_home = bothome.BotHome()
+bot_home.setBot(bot)
+
+db_funcs = dbfuncs.DatabaseFuncs(bot)
+bot_funcs = botfuncs.BotFuncs(bot)
+
+start_keyboard = telebot.types.ReplyKeyboardMarkup(True)
+start_keyboard.row('Занять переговорку', 'Изменить запись', 'Удалить запись')
+start_keyboard.row('Моя занятость', 'Занятость переговорки')
+start_keyboard.row('Дата', 'Справка', 'Кот')
 
 
-def main():
-    new_offset = None
-    today = now.day
-    hour = now.hour
-
-    while True:
-        greet_bot.get_updates(new_offset)
-
-        last_update = greet_bot.get_last_update()
-
-        last_update_id = last_update['update_id']
-        last_chat_message = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-
-        if last_chat_message.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            today += 1
-        elif last_chat_message.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
-        elif last_chat_message.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
-
-        new_offset = last_update_id + 1
+# ------------------------------------------------ #
+@bot.message_handler(commands=['start', 'help', 'add', 'time', 'all', 'my', 'delete', 'update', 'cat'])
+def startFunc(message):
+    if bot_funcs.isUserExist(message):
+        command = str(message.text).strip()
+        if command == '/start':
+            bot.send_message(message.chat.id, 'Я тебя уже знаю, ' + message.from_user.username,
+                             reply_markup=start_keyboard)
+        elif command == '/help':
+            bot_funcs.printHelp(message)
+        elif command == '/add':
+            bot_funcs.regTime(message)
+        elif command == '/time':
+            bot_funcs.printToday(message.chat.id, datetime.datetime.today())
+        elif command == '/all':
+            bot_funcs.printAllTimes(message)
+        elif command == '/delete':
+            bot_funcs.seeTimesListFor(message, 1)
+        elif command == '/update':
+            bot_funcs.seeTimesListFor(message, 2)
+        elif command == '/my':
+            bot_funcs.printMyTimes(message)
+        elif command == '/cat':
+            bot.send_message(message.chat.id, 'Мяу', reply_markup=start_keyboard)
 
 
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    print(message)
+    if bot_funcs.isUserExist(message):
+        user_message = message.text.lower()
+        chat_id = message.chat.id
+        user_first_name = message.from_user.first_name
+        today = datetime.datetime.today()
+        now_day = today.day
 
+        if user_message == 'привет':
+            bot.send_message(chat_id, 'Привет, ' + user_first_name)
+        elif user_message == 'пока':
+            bot.send_message(chat_id, 'Удачи тебе, ' + user_first_name)
+        elif user_message == 'дата':
+            bot_funcs.printToday(chat_id, today)
+        elif user_message == 'моя занятость':
+            bot_funcs.printMyTimes(message)
+        elif user_message == 'удалить запись':
+            bot_funcs.seeTimesListFor(message, 1)
+        elif user_message == 'изменить запись':
+            bot_funcs.seeTimesListFor(message, 2)
+        elif user_message == 'занять переговорку':
+            bot_funcs.regTime(message)
+        elif user_message == 'занятость переговорки':
+            bot_funcs.printAllTimes(message)
+        elif user_message == 'справка':
+            bot_funcs.printHelp(message)
+        elif user_message == 'кот':
+            bot.send_message(message.chat.id, 'Мяу', reply_markup=start_keyboard)
+
+
+# @bot.message_handler(content_types=['sticker'])
+# def sticker_id(message):
+#     print(message)
+
+
+# ------------------------------------------------ #
+server = Flask(__name__)
+
+
+@server.route("/{}".format(config.TOKEN), methods=['POST'])
+def getMessage():
+    updates = bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
+    print(updates)
+
+    return "!", 200
+
+
+@server.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    bot.remove_webhook()
+    s = bot.set_webhook('{URL}{HOOK}'.format(URL=config.URL, HOOK=config.TOKEN))
+    if s:
+        return "webhook setup ok", 200
+    else:
+        return "webhook setup failed", 200
+
+
+@server.route('/')
+def index():
+    return '.'
+
+
+# ------------------------------------------------ #
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
