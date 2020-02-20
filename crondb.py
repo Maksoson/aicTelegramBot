a41ca7e2fc8d7ea126ae4fae962febbf2562fcc3
Краснопер Максим <maxibon_17@mail.ru>
2020-02-21 00:12:50 +0300

    10000 commits lets go

--- crondb.py
+++ crondb.py
@@ -10,9 +10,9 @@ scheduler = BlockingScheduler()
 db_funcs = dbfuncs.DatabaseFuncs(bot)
 
 
-@scheduler.scheduled_job('cron', day_of_week='0-6', hour=0, minute=15)
+@scheduler.scheduled_job('cron', day_of_week='0-6', hour=0, minute=17)
 def scheduled_job():
-    db_funcs.deleteOldTimes()
-
+    # db_funcs.deleteOldTimes()
+    print('qq')
 
 scheduler.start()
