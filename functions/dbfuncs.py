83ca9dd766bd5b8de4c361bc24190f794e28cae5
Максим Краснопер <maxibon_17@mail.ru>
2020-03-04 17:30:38 +0300

    10000 commits lets go

--- functions/dbfuncs.py
+++ functions/dbfuncs.py
@@ -154,8 +154,8 @@ class DatabaseFuncs:
             with connection.cursor() as cursor:
                 query = 'SELECT public.users.*, public.timetable.* FROM public.timetable ' \
                         'INNER JOIN public.users ON public.users.id = public.timetable.user_id ' \
-                        'WHERE (public.timetable.day_use >= %s AND public.month_use = %s) ' \
-                        'OR (public.timetable.day_use < %s AND public.month_use > %s) '
+                        'WHERE (public.timetable.day_use >= %s AND public.timetable.month_use = %s) ' \
+                        'OR (public.timetable.day_use < %s AND public.timetable.month_use > %s) '
                 cursor.execute(query, [datetime.datetime.today().day, datetime.datetime.today().month,
                                        datetime.datetime.today().day, datetime.datetime.today().month])
 
